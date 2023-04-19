eaac29b32044814558dbde6b5ced29bd1165fa0c
Gen Tamada <gtamada@ucsb.edu>
2023-04-19 05:26:51 -0700

    Update cli.py

--- openunmix/cli.py
+++ openunmix/cli.py
@@ -4,205 +4,110 @@ import torchaudio
 import json
 import numpy as np
 import tqdm
+import streamlit as st
 
 import utils
 import predict
 import data
 
-import argparse
+
+# !python openunmix/cli.py \
+#   gdrive/MyDrive/AudioSeparation/songs/Another.wav \
+#   --model gdrive/MyDrive/AudioSeparation/gen_model_new/ \
+#   --outdir gdrive/MyDrive/AudioSeparation/output/ \
+#   --targets vocals drums bass \
+#   --residual other \
+#   --verbose
 
 
-def separate():
-    parser = argparse.ArgumentParser(
-        description="UMX Inference",
-        add_help=True,
-        formatter_class=argparse.RawDescriptionHelpFormatter,
-    )
+class separator_args:
+    def __init__(self, model, targets, outdir, ext, start, duration, no_cuda, audio_backend, niter, wiener_win_len, residual, aggregate, filterbank, verbose):
+        self.model = model
+        self.targets = targets
+        self.outdir = outdir
+        self.ext = ext
+        self.start = start
+        self.duration = duration
+        self.no_cuda = no_cuda
+        self.audio_backend = audio_backend
+        self.niter = niter
+        self.wiener_win_len = wiener_win_len
+        self.residual = residual
+        self.aggregate = aggregate
+        self.filterbank = filterbank
+        self.verbose = verbose
 
-    parser.add_argument("input", type=str, nargs="+", help="List of paths to wav/flac files.")
+args = separator_args(
+    model = "pre-trained/",
+    targets = ["vocals", "drums", "bass"],
+    outdir = "output/",
+    ext = ".wav",
+    start = 0.0,
+    duration = None,
+    no_cuda = False,
+    audio_backend = None,
+    niter = 1,
+    wiener_win_len = 300,
+    residual = "other",
+    aggregate = None,
+    filterbank = "torch",
+    verbose = True
+)
 
-    parser.add_argument(
-        "--model",
-        default="umxl",
-        type=str,
-        help="path to mode base directory of pretrained models, defaults to UMX-L",
-    )
+use_cuda = torch.cuda.is_available()
+device = torch.device("cuda" if use_cuda else "cpu")
 
-    parser.add_argument(
-        "--targets",
-        nargs="+",
-        type=str,
-        help="provide targets to be processed. \
-              If none, all available targets will be computed",
-    )
+separator = utils.load_separator(
+    model_str_or_path=args.model,
+    targets=args.targets,
+    niter=args.niter,
+    residual=args.residual,
+    wiener_win_len=args.wiener_win_len,
+    device=device,
+    pretrained=True,
+    filterbank=args.filterbank,
+)
+@st.cache
 
-    parser.add_argument(
-        "--outdir",
-        type=str,
-        help="Results path where audio evaluation results are stored",
-    )
 
-    parser.add_argument(
-        "--ext",
-        type=str,
-        default=".wav",
-        help="Output extension which sets the audio format",
-    )
+def separate(audio, rate):
 
-    parser.add_argument("--start", type=float, default=0.0, help="Audio chunk start in seconds")
-
-    parser.add_argument(
-        "--duration",
-        type=float,
-        help="Audio chunk duration in seconds, negative values load full track",
-    )
-
-    parser.add_argument(
-        "--no-cuda", action="store_true", default=False, help="disables CUDA inference"
-    )
-
-    parser.add_argument(
-        "--audio-backend",
-        type=str,
-        help="Sets audio backend. Default to torchaudio's default backend: See https://pytorch.org/audio/stable/backend.html"
-        "(`sox_io`, `sox`, `soundfile` or `stempeg`)",
-    )
-
-    parser.add_argument(
-        "--niter",
-        type=int,
-        default=1,
-        help="number of iterations for refining results.",
-    )
-
-    parser.add_argument(
-        "--wiener-win-len",
-        type=int,
-        default=300,
-        help="Number of frames on which to apply filtering independently",
-    )
-
-    parser.add_argument(
-        "--residual",
-        type=str,
-        default=None,
-        help="if provided, build a source with given name "
-        "for the mix minus all estimated targets",
-    )
-
-    parser.add_argument(
-        "--aggregate",
-        type=str,
-        default=None,
-        help="if provided, must be a string containing a valid expression for "
-        "a dictionary, with keys as output target names, and values "
-        "a list of targets that are used to build it. For instance: "
-        '\'{"vocals":["vocals"], "accompaniment":["drums",'
-        '"bass","other"]}\'',
-    )
-
-    parser.add_argument(
-        "--filterbank",
-        type=str,
-        default="torch",
-        help="filterbank implementation method. "
-        "Supported: `['torch', 'asteroid']`. `torch` is ~30%% faster "
-        "compared to `asteroid` on large FFT sizes such as 4096. However "
-        "asteroids stft can be exported to onnx, which makes is practical "
-        "for deployment.",
-    )
-    parser.add_argument(
-        "--verbose",
-        action="store_true",
-        default=False,
-        help="Enable log messages",
-    )
-    args = parser.parse_args()
-
-    if args.audio_backend != "stempeg" and args.audio_backend is not None:
-        torchaudio.set_audio_backend(args.audio_backend)
-
-    use_cuda = not args.no_cuda and torch.cuda.is_available()
-    device = torch.device("cuda" if use_cuda else "cpu")
     if args.verbose:
         print("Using ", device)
     # parsing the output dict
-    aggregate_dict = None if args.aggregate is None else json.loads(args.aggregate)
+    aggregate_dict = None
 
     # create separator only once to reduce model loading
     # when using multiple files
-    separator = utils.load_separator(
-        model_str_or_path=args.model,
-        targets=args.targets,
-        niter=args.niter,
-        residual=args.residual,
-        wiener_win_len=args.wiener_win_len,
-        device=device,
-        pretrained=True,
-        filterbank=args.filterbank,
-    )
 
     separator.freeze()
     separator.to(device)
 
-    if args.audio_backend == "stempeg":
-        try:
-            import stempeg
-        except ImportError:
-            raise RuntimeError("Please install pip package `stempeg`")
-
     # loop over the files
-    for input_file in tqdm.tqdm(args.input):
-        if args.audio_backend == "stempeg":
-            audio, rate = stempeg.read_stems(
-                input_file,
-                start=args.start,
-                duration=args.duration,
-                sample_rate=separator.sample_rate,
-                dtype=np.float32,
-            )
-            audio = torch.tensor(audio)
-        else:
-            audio, rate = data.load_audio(input_file, start=args.start, dur=args.duration)
-        estimates = predict.separate(
-            audio=audio,
-            rate=rate,
-            aggregate_dict=aggregate_dict,
-            separator=separator,
-            device=device,
-        )
-        if not args.outdir:
-            model_path = Path(args.model)
-            if not model_path.exists():
-                outdir = Path(Path(input_file).stem + "_" + args.model)
-            else:
-                outdir = Path(Path(input_file).stem + "_" + model_path.stem)
-        else:
-            outdir = Path(args.outdir) / Path(input_file).stem
-        outdir.mkdir(exist_ok=True, parents=True)
+    estimates = predict.separate(
+        audio=audio,
+        rate=rate,
+        aggregate_dict=aggregate_dict,
+        separator=separator,
+        device=device,
+    )
+    for target, estimate in estimates.items():
+        audio_data = torch.squeeze(estimate).to("cpu").numpy()
+        st.write(f"Playing {target}:")
+        st.audio(audio_data, format='audio/wav', sample_rate=separator.sample_rate)
 
-        # write out estimates
-        if args.audio_backend == "stempeg":
-            target_path = str(outdir / Path("target").with_suffix(args.ext))
-            # convert torch dict to numpy dict
-            estimates_numpy = {}
-            for target, estimate in estimates.items():
-                estimates_numpy[target] = torch.squeeze(estimate).detach().cpu().numpy().T
 
-            stempeg.write_stems(
-                target_path,
-                estimates_numpy,
-                sample_rate=separator.sample_rate,
-                writer=stempeg.FilesWriter(multiprocess=True, output_sample_rate=rate),
-            )
-        else:
-            for target, estimate in estimates.items():
-                target_path = str(outdir / Path(target).with_suffix(args.ext))
-                torchaudio.save(
-                    target_path,
-                    torch.squeeze(estimate).to("cpu"),
-                    sample_rate=separator.sample_rate,
-                )
+uploaded_file = st.file_uploader(
+    label="Choose a wav file to separate",
+    type="wav",
+    accept_multiple_files=False,
+    key="audio_file_uploader",
+    help="Upload a wav file to separate into 4 tracks",
+)
 
-if __name__ == "__main__":
-    separate()
+if uploaded_file is not None:
+    audio, rate = torchaudio.load(uploaded_file, format="wav")
+    st.write("File uploaded.")
+    separate(audio, rate)
+else:
+    st.write("No file uploaded.")
